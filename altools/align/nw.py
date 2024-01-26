e9209914c0f55c68211835aa0b23ba3fb0d87590
Allison Coomber <alc266@cornell.edu>
2024-01-25 22:38:22 -0500

    Implemented get alignment from paths

--- altools/align/nw.py
+++ altools/align/nw.py
@@ -1,5 +1,6 @@
 from altools.align.alignment import GlobalAlignment
 from altools.dna_sequence import DNASequence
+from altools.nucleotide import Nucleotide
 
 import numpy as np
 
@@ -9,7 +10,7 @@ def align_nw(
     match_reward: int = 1,
     mismatch_penalty: int = -2,
     gap_penalty: int = -1,
-) -> GlobalAlignment:
+) -> list[GlobalAlignment]:
     mat = np.zeros(((len(seq_a) + 1, len(seq_b) + 1)), dtype=int)
     mat[0, :] = [-1 * i for i in range(0, len(seq_a) + 1)]  # seq_a goes along the top
     mat[:, 0] = [-1 * i for i in range(0, len(seq_b) + 1)]  # seq_b goes down the rows
@@ -38,15 +39,9 @@ def align_nw(
             traceback[(i, j)] = max_indices
 
     traceback_pos = (len(seq_b), len(seq_a))
-    # if it is a two, i-1, j-1
-    # if it is a 1 i, j-1
-    # it it is a 0 i-1, j
-    ## what about when there are two options
-    ## should we return multiple best alignments? - split cases with multiple into new cases?
-    breakpoint()
-    traces = [traceback_pos]
-    while traceback_pos is not (0, 0):
-        pass
+
+    paths = get_paths(traceback, traceback_pos)
+    return get_alignments_from_paths(paths, seq_a, seq_b)
 
 
 def pos_from_traceback(
@@ -73,13 +68,50 @@ def get_paths(traceback: dict[tuple[int, int], np.ndarray], position: tuple[int,
             result.append(sub_path + [position])
     return result
 
+def get_alignments_from_paths(paths: list[list[tuple[int, int]]], seq_a: DNASequence, seq_b: DNASequence) -> list[GlobalAlignment]:
+    # start at the first tuple in the path
+    # this corresponds to the index+1 of the sequences
+    # where i is the seq_b and j is seq_a
+    # follow the path, building up both sequences as we go
+    # if the indices change the same amount, get the nucleotide at that position from both sequences
+    # if there is a left gap, then seq_b has a gap
+    # if there is top gap, then seq_a has a gap
+    # okay this is three branches, is there a way to make it less branch-y
+    # i think yes if instead of making "three cases" we just have one situation 
+    # where we compare to the previous value each time?
+    alignments = []
+    for path in paths:
+        aligned_a = []
+        aligned_b = []
+        for idx in range(len(path)):
+            i_current, j_current = path[idx]
+            if idx == 0:
+                difference_in_position = (i_current, j_current)
+            else:
+                i_previous, j_previous = path[idx-1]
+                difference_in_position = (i_current - i_previous, j_current - j_previous)
+            if difference_in_position == (1,1):
+                aligned_a.append(seq_a[j_current-1])
+                aligned_b.append(seq_b[i_current-1])
+            elif difference_in_position == (0,1):
+                aligned_a.append(seq_a[j_current-1])
+                aligned_b.append(Nucleotide.GAP)  
+            elif difference_in_position == (1,0):
+                aligned_a.append(Nucleotide.GAP)
+                aligned_b.append(seq_b[i_current-1])
+        alignments.append(GlobalAlignment(DNASequence(aligned_a), DNASequence(aligned_b)))
+    return alignments             
+            
+            
 
 
 if __name__ == "__main__":
-    align_nw(
+    alignments = align_nw(
         DNASequence.from_string("GCATGCG"),
         DNASequence.from_string("GATTACA"),
         match_reward=1,
         mismatch_penalty=-1,
         gap_penalty=-1,
     )
+    for alignment in alignments:
+        print(alignment)
