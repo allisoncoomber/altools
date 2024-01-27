00d332531f0506667e30ce623289d845cf5e0b05
Allison Coomber <alc266@cornell.edu>
2024-01-26 23:00:07 -0500

    Implemented test for align nw

---
+++ tests/test_align_nw.py
@@ -0,0 +1,28 @@
+from altools.align.alignment import GlobalAlignment
+from altools.align.nw import align_nw
+from altools.dna_sequence import DNASequence
+
+
+def test_align_nw():
+    alignments = align_nw(
+        DNASequence.from_string("GCATGCG"),
+        DNASequence.from_string("GATTACA"),
+        match_reward=1,
+        mismatch_penalty=-1,
+        gap_penalty=-1,
+    )
+    expected_alignments = [
+        GlobalAlignment(
+            DNASequence.from_string("GCATG-CG"), DNASequence.from_string("G-ATTACA")
+        ),
+        GlobalAlignment(
+            DNASequence.from_string("GCA-TGCG"), DNASequence.from_string("G-ATTACA")
+        ),
+        GlobalAlignment(
+            DNASequence.from_string("GCAT-GCG"), DNASequence.from_string("G-ATTACA")
+        ),
+    ]
+
+    for alignment in alignments:
+        assert alignment in expected_alignments
+    assert len(alignments) == len(expected_alignments)
